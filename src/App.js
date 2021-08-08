03da3db05426e894b1e720baa5893537424fed89
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-08 11:21:32 +0430

    feat: send form elements data to FormBuilder component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FormBuilder from "./components/FormBuilder";
+import { getFormElements } from "./form-elements-data/FormElements";
+
 import "./App.css";
 const App = () => {
+  const [formElements, setFormElements] = useState([]);
+  useEffect(() => {
+    setFormElements(getFormElements());
+  }, []);
   return (
     <div className="app-container">
       <div className="form-container">
-        <FormBuilder />
+        <FormBuilder formElements={formElements} />
       </div>
     </div>
   );
