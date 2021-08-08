f7eeac23286b3ee37e410fbe3ee6434c39876fb2
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-08 15:43:48 +0430

    refactor: use form elements data into FormBuilder
    Using form elements data and checking them based on the type of element

--- src/components/FormBuilder.jsx
+++ src/components/FormBuilder.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import InputFormGroup from "./InputFormGroup";
 import InputFormRadio from "./InputFormRadio";
 import InputFormSelect from "./InputFormSelect";
-const FormBuilder = () => {
+const FormBuilder = ({ formElements }) => {
   const [formData, setFormData] = useState({});
   const handleChange = (e) => {
     const newFormData = { ...formData };
@@ -10,63 +10,48 @@ const FormBuilder = () => {
     setFormData(newFormData);
   };
 
-  console.log(formData);
-
   return (
     <form>
-      <InputFormGroup
-        name="firstname"
-        id="firstname"
-        label="FirstName:"
-        onChange={handleChange}
-      />
+      {formElements.map((formElement) => {
+        if (formElement.elementType === "input-form-group") {
+          return (
+            <InputFormGroup
+              key={formElement.key}
+              name={formElement.name}
+              id={formElement.id}
+              label={formElement.label}
+              onChange={handleChange}
+            />
+          );
+        }
 
-      <InputFormGroup
-        name="lastname"
-        id="lastname"
-        label="Lastname:"
-        onChange={handleChange}
-      />
+        if (formElement.elementType === "input-form-radio") {
+          return (
+            <InputFormRadio
+              key={formElement.key}
+              name={formElement.name}
+              id={formElement.id}
+              type={formElement.type}
+              caption={formElement.caption}
+              options={formElement.options}
+              onChange={handleChange}
+            />
+          );
+        }
 
-      <InputFormGroup
-        name="age"
-        id="age"
-        label="Age:"
-        onChange={handleChange}
-      />
-
-      <InputFormGroup
-        name="email"
-        id="email"
-        label="Email:"
-        onChange={handleChange}
-      />
-
-      <InputFormRadio
-        name="gender"
-        id="gender"
-        type="radio"
-        caption="What's your gender?"
-        options={[
-          { key: 1, value: "male", label: "Male" },
-          { key: 2, value: "female", label: "Female" },
-        ]}
-        onChange={handleChange}
-      />
-
-      <InputFormSelect
-        name="state"
-        id="state"
-        caption="Which state do you live in?"
-        options={[
-          { key: 1, value: "california", label: "California" },
-          { key: 2, value: "texas", label: "Texas" },
-          { key: 3, value: "florida", label: "Florida" },
-          { key: 4, value: "newyork", label: "Newyork" },
-          { key: 5, value: "pennsylvania", label: "Pennsylvania" },
-        ]}
-        onChange={handleChange}
-      />
+        if (formElement.elementType === "input-form-select") {
+          return (
+            <InputFormSelect
+              key={formElement.key}
+              name={formElement.name}
+              id={formElement.id}
+              caption={formElement.caption}
+              options={formElement.options}
+              onChange={handleChange}
+            />
+          );
+        }
+      })}
 
       <br />
       <button type="submit" className="btn btn-primary">
