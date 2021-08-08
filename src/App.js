6b9a3dc39b7edcaa8a7ac0fda71e163cf3db68c8
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-08 16:00:11 +0430

    feat: add handleError and doSubmit frunctions
    If there are no errors based on the handleError field into every element object, formData will be returned

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 import FormBuilder from "./components/FormBuilder";
 import { getFormElements } from "./form-elements-data/FormElements";
 
-import "./App.css";
 const App = () => {
   const [formElements, setFormElements] = useState([]);
+  const [returnedFormData, setReturnedFormData] = useState([]);
+
   useEffect(() => {
     setFormElements(getFormElements());
   }, []);
+
+  const handleSubmit = (formData) => {
+    setReturnedFormData(formData);
+    alert("Data Returned Successfully");
+    console.log("Returned Data", returnedFormData);
+  };
+
   return (
     <div className="app-container">
       <div className="form-container">
-        <FormBuilder formElements={formElements} />
+        <FormBuilder formElements={formElements} onSubmit={handleSubmit} />
       </div>
     </div>
   );
--- src/components/FormBuilder.jsx
+++ src/components/FormBuilder.jsx
@@ -2,16 +2,83 @@ import React, { useState } from "react";
 import InputFormGroup from "./InputFormGroup";
 import InputFormRadio from "./InputFormRadio";
 import InputFormSelect from "./InputFormSelect";
-const FormBuilder = ({ formElements }) => {
+const FormBuilder = ({ formElements, onSubmit }) => {
   const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+
   const handleChange = (e) => {
     const newFormData = { ...formData };
     newFormData[e.currentTarget.name] = e.currentTarget.value;
     setFormData(newFormData);
   };
 
+  function doSubmit(e) {
+    e.preventDefault();
+    const errors = handleError(formElements);
+    setErrors(errors || {});
+    if (errors) return; // if there are no errors, formData will be returned
+    onSubmit(formData);
+  }
+
+  function handleError(formElements) {
+    const returnedErrors = {};
+
+    formElements.map((formElement) => {
+      formElement.handleError.map((item) => {
+        if (item.isRequired) {
+          if (!formData[formElement.name]) {
+            returnedErrors[
+              formElement.name
+            ] = `Your ${formElement.name} is Required`;
+          }
+        }
+
+        if (item.maxLength) {
+          if (
+            formData[formElement.name] &&
+            formData[formElement.name].length > item.maxLength
+          ) {
+            returnedErrors[
+              formElement.name
+            ] = `Your ${formElement.name} can not be more than ${item.maxLength} characters`;
+          }
+        }
+
+        if (item.isEmail) {
+          if (formData[formElement.name]) {
+            const emailRe =
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            const isEmail = emailRe.test(
+              String(formData[formElement.name]).toLowerCase()
+            );
+            if (isEmail === false) {
+              returnedErrors[
+                formElement.name
+              ] = `Please enter your email address in correct format`;
+            }
+          }
+        }
+
+        if (item.maxValue) {
+          if (
+            formData[formElement.name] &&
+            formData[formElement.name] > item.maxValue
+          ) {
+            returnedErrors[
+              formElement.name
+            ] = `Your ${formElement.name} can not be more than ${item.maxValue} `;
+          }
+        }
+      });
+    });
+
+    return Object.keys(returnedErrors).length === 0 ? null : returnedErrors;
+  }
+
+  console.log(errors);
+
   return (
-    <form>
+    <form onSubmit={doSubmit}>
       {formElements.map((formElement) => {
         if (formElement.elementType === "input-form-group") {
           return (
